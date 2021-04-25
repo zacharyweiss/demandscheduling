629cce0b37d954c323d1504f05c469536101d805
Zachary Weiss <zachary.weiss.acct@gmail.com>
2021-04-25 15:52:29 -0400

    Pycharm project creation

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyomo.environ as pyomo
+import numpy as np
+
+
+def main():
+    print(f'Hi')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
