eb2450eebe0dd49aba0d3d48f72dc19b55235f8b
Zachary Weiss <zachary.weiss.acct@gmail.com>
2021-04-26 18:09:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import pyomo.environ as pyomo
 import numpy as np
 
 
+# global settings
+N_HOURS = 24
+
+
 def main():
-    print(f'Hi')
+    model = pyomo.ConcreteModel()
+
+    hours = range(N_HOURS)
+    prices = np.random.rand(N_HOURS)*10
+    loads = np.random.rand(N_HOURS)*2
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     main()
