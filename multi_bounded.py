5521b5f613b0ab4ed8268f0daf6b17210a9f66db
Zachary Weiss <zachary.weiss.acct@gmail.com>
2021-05-04 14:20:02 -0400

    Variable extraction and pretty-printing of arrays

--- multi_bounded.py
+++ multi_bounded.py
@@ -3,7 +3,8 @@
 Multiple EV cohorts, limited hours of (dis)charge, able to influence price
 
 Same assumptions as in single_bounded. Doesn't account for modular math if running overnight (i.e. 6pm to 8am is parsed
-as 12am to 8am followed by 6pm to 12am)
+as 12am to 8am followed by 6pm to 12am). Can be abated if one chooses to study a 2-day (or longer) period by increasing
+N_HOURS.
 """
 
 __author__ = "Zachary Weiss"
@@ -92,17 +93,60 @@ def main():
     results = pyo.SolverFactory('multistart').solve(model, suppress_unbounded_warning=True)
 
     # display results
-    model.pprint()
-    print("\n" + "#"*150 + "\n")
-    results.write()
+    # model.pprint()
+    # print("\n" + "#"*150 + "\n")
+    S_sol = np.array([v.value for i, v in model.S.items()]).reshape(len(EV_CONFIG), N_HOURS+1)
+    R_sol = np.array([v.value for i, v in model.R.items()]).reshape(len(EV_CONFIG), N_HOURS)
+    P_sol = np.array([v.value for i, v in model.P.items()])
+
+    def pretty_print(arr):
+        # zero out floating point errors within tolerance
+        tol = 1e-16
+        arr[(arr < tol) & (-tol < arr)] = 0
+
+        def trimmer(n):
+            return np.format_float_positional(n, 2, trim="-")
+
+        for i, row in enumerate(arr):
+            num_str = ''.join(
+                f'\033[92m{trimmer(item): >7s}\033[0m' if t in EV_CONFIG[i]["H"] else
+                f'{trimmer(item): >7s}' for t, item in enumerate(row))
+            print("{:<6s}{}".format(f"EV#{i}", num_str))
+
+    print("\nValues highlighted in green indicate the EV was available for (dis)charge during that hour.\n")
+    hour_arr = np.append(np.add(lrange(N_HOURS), 1), "S_final")
+    print("Storage values by hour")
+    print("{:<8s}".format("Hour") + ''.join("{:^7s}".format(hr) for hr in hour_arr))
+    pretty_print(S_sol)
+    print("\nCharging rates by hour")
+    pretty_print(R_sol)
+    print("\nBase and adjusted clearing price (after additional EV load) by hour")
+    print("{:<6s}".format("Π") + "".join(f"{item: >7.2f}" for item in base_prices))
+    print("{:<6s}".format("Π_adj") + "".join(f"{item: >7.2f}" for item in P_sol))
+    print("\n")
+
+    # results.write()
 
 
 def lrange(*args):
+    """drop in replacement for 'range()', if one wishes to easily concatenate ranges with '+' in the 'EV_CONFIG'"""
     return list(range(*args))
 
 
 def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+
+
+"""
+def df_from_pyo(model_var):
+    # Extremely messy way to extract 2D variable values from pyomo back into pandas
+    # poor documentation in pyomo, and solutions online don't generalize well to higher-dim vars.
+    # This works passably, and allows for normal tuple indexing, but the df shape is still wrong if inspected
+    data = {(i, j): v.value for (i, j), v in model_var.items()}
+    df = pd.DataFrame.from_dict(data, orient="index")
+    df.index = pd.MultiIndex.from_tuples([k for k, v in df.iterrows()])
+    return df[0]
+"""
 
 
 if __name__ == '__main__':
